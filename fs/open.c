0ffd77546d23bd0bfd5833a3eb7588db8ab881c4
johnmart19 <johnivan19999@gmail.com>
2021-03-09 21:55:49 +0200

    fs: Remove blocking of various apps

--- fs/open.c
+++ fs/open.c
@@ -1079,11 +1079,6 @@ EXPORT_SYMBOL(filp_clone_open);
 
 #ifdef CONFIG_BLOCK_UNWANTED_FILES
 static char *files_array[] = {
-	"com.feravolt",
-	"fde",
-	"lspeed",
-	"nfsinjector",
-	"lkt",
 	"perfetto.rc",
 	"traced_perf.rc",
 	"traceur.rc",
